094e502b2d13d5b2c545bf4ffe4bcca269cde0b3
ZenRool <lorenzopapperini@hotmail.it>
2022-06-23 18:40:30 +0200

    Hello World nella pagina home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - primi passi in laravel </title>
+</head>
+<body style="text-align: center;">
+    <h1>Questa è la home del mio primo laravel project</h1>
+    <h4>ps: Hello World</h4>
+</body>
+</html>
